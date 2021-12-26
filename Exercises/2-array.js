d3fa7699a64331b352e63feb01f6e7ca089fb469
gsvcrypto <gsvcrypto@gmail.com>
2021-12-26 10:55:49 +0200

    Labs 2, 3 are done

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const get = (i) => arr[i];
+  get.push = (x) => arr.push(x);
+  get.pop = () => arr.pop();
+  return get;
+};
 
 module.exports = { array };
